78a35d84cf716713a697d19da4071bf98dd7144f
Anastasiia <nastasiia.kletsko@gmail.com>
2026-02-08 19:35:06 +0200

    v1

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,9 +2,33 @@ import { Note } from "../models/note.js";
 import createHttpError from "http-errors";
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+  // Отримуємо параметри пагінації
+  // і задаємо дефолтні значення
+  const { page = 1, perPage = 10 } = req.query;
+
+  const skip = (page - 1) * perPage;
+
+  // Створюємо базовий запит до колекції
+  const notesQuery = Note.find();
+
+  // Виконуємо одразу два запити паралельно
+  const [totalNotes, notes] = await Promise.all([
+    notesQuery.clone().countDocuments(),
+    notesQuery.skip(skip).limit(perPage),
+  ]);
+
+	// Обчислюємо загальну кількість «сторінок»
+  const totalPages = Math.ceil(totalNotes / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalNotes,
+    totalPages,
+    notes,
+  });
 };
+
 
 export const getNoteById = async (req, res, next) => {
   const { noteId } = req.params;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { errors } from "celebrate";
 import { connectMongoDB } from "./db/connectMongoDB.js";
 import { logger } from "./middleware/logger.js";
 import { notFoundHandler } from "./middleware/notFoundHandler.js";
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(notesRoutes);
+
+app.use(errors());
 
 app.use(notFoundHandler);
 
