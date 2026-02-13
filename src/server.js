d42834fe98c2a883e18ed4db29243e47b32c575a
Anastasiia <nastasiia.kletsko@gmail.com>
2026-02-13 22:42:38 +0200

    v3

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -23,6 +24,7 @@ app.use(helmet());
 app.use(cookieParser());
 
 app.use(authRoutes);
+app.use(userRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
